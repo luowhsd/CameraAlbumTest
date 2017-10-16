ec03f119e9efdc1bb941a12549c7e55da61ef942
luowh <luowh@chinasap.cn>
2017-10-16 17:54:32 +0800

    Signed-off-by: luowh <luowh@chinasap.cn> 调用照片选择器

--- app/src/main/java/com/example/fangl/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/fangl/cameraalbumtest/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.fangl.cameraalbumtest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -25,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button takePhoto = (Button)findViewById(R.id.take_photo);
+        Button choosePhoto = (Button)findViewById(R.id.choose_from_album);
         imageView = (ImageView)findViewById(R.id.picture);
+
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,6 +65,18 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        choosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else{
+                    openAlbum();
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -75,4 +95,29 @@ public class MainActivity extends AppCompatActivity {
             default:
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else{
+                    Toast.makeText(this, "已禁用", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+
+
+        }
+
+    }
+
+    // 打开图片选择器
+    private void openAlbum(){
+
+
+
+    }
+
 }
