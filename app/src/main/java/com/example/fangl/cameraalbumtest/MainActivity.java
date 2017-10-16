b7dfe086c826926ff36414542e3b5edeca9f2fe5
luowh <luowh@chinasap.cn>
2017-10-16 17:16:29 +0800

    Signed-off-by: luowh <luowh@chinasap.cn>

---
+++ app/src/main/java/com/example/fangl/cameraalbumtest/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.fangl.cameraalbumtest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int TAKE_PHOTO = 1;
+    private ImageView imageView;
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button takePhoto = (Button)findViewById(R.id.take_photo);
+        imageView = (ImageView)findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
+                try {
+
+                    if(outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT>=24){
+                    imageUri = FileProvider.getUriForFile(MainActivity.this,"com.example.fangl.cameraalbumtest.fileprovider",outputImage);
+                }else{
+                    imageUri = Uri.fromFile(outputImage);
+                }
+
+                // 启动相机程序
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
+
+
+            }
+
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (resultCode==RESULT_OK){
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imageView.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+        }
+    }
+}
