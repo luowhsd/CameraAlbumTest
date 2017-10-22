1d8d552611cb7734127245a2506652670519efe9
luowhsd <591151328@qq.com>
2017-10-22 22:27:48 +0800

    Signed-off-by: luowhsd <591151328@qq.com> CHOOSE_PHOTO

--- app/src/main/java/com/example/fangl/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/fangl/cameraalbumtest/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.fangl.cameraalbumtest;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -95,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case CHOOSE_PHOTO:
-                if(requestCode==RESULT_OK){
+                if(resultCode==RESULT_OK){
 
                     if(Build.VERSION.SDK_INT>=19){
                         //4.4及其以上系统使用
@@ -139,9 +142,68 @@ public class MainActivity extends AppCompatActivity {
     @TargetApi(19)
     private void handleImageOnKitKat(Intent data){
 
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            // 如果是document类型的uri，通过document的id来处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+
+                String id = docId.split(":")[1];  // 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+
+            }else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+
+
+
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+
+            // 如果是content类型的uri，使用普通方式处理
+            imagePath = getImagePath(uri,null);
+
+        }else if("file".equalsIgnoreCase(uri.getScheme())){
+
+            imagePath = uri.getPath();
+
+        }
+        displayImage(imagePath);
+
+
     }
 
     private void handleImageBeforeKitKat(Intent data){
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor !=null){
+
+            if(cursor.moveToNext()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+
+
+        return path;
+    }
+
+    private void displayImage(String imagePath){
+        if(imagePath!=null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            imageView.setImageBitmap(bitmap);
+        }else{
+            Toast.makeText(this,"failed to get image",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
